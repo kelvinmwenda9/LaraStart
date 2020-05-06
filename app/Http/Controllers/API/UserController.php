85b5ad1afd5cd59ad1a032d6e8a049739f9c8f7c
kelvinmwenda9 <kelvinmwenda9@gmail.com>
2020-05-06 11:37:56 +0300

    Server Validation, Insert DB, Display on Table

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
@@ -15,6 +18,7 @@ class UserController extends Controller
     public function index()
     {
         //
+        return User::latest()->paginate(10);
     }
 
     /**
@@ -26,6 +30,20 @@ class UserController extends Controller
     public function store(Request $request)
     {
         //
+        $this->validate($request,[
+            'name' => 'required|string|max:191',
+            'email' => 'required|string|email|max:191|unique:users',
+            'password' => 'required|string|min:6'
+        ]);
+
+        return User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'type' => $request['type'],
+            'bio' => $request['bio'],
+            'photo' => $request['photo'],
+            'password' => Hash::make($request['password']),
+        ]);
     }
 
     /**
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -16,29 +16,32 @@
                 <table class="table table-hover">
                   <thead>
                     <tr>
-                      <th>ID</th>
-                      <th>Name</th>
-                      <th>Email</th>
-                      <th>Type</th>
-                      <th>Modify</th>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Type</th>
+                        <th>Registered At</th>
+                        <th>Modify</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>183</td>
-                      <td>John Doe</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-success">Approved</span></td>
-                      <td>
-                          <a href="">
-                              <i class="fa fa-edit blue"></i>
-                          </a>
 
-                           <a href="">
-                              <i class="fa fa-trash red"></i>
-                          </a>
+                    <tr v-for="user in users" :key="user.id">
+                        <td>{{ user.id }}</td>
+                        <td>{{ user.name }}</td>
+                        <td>{{ user.email }}</td>
+                        <td>{{ user.type }}</td>
+                        <td>{{ user.created_at }}</td>
+                        <td>
+                            <a href="">
+                                <i class="fa fa-edit blue"></i>
+                            </a>
 
-                      </td>
+                            <a href="">
+                                <i class="fa fa-trash red"></i>
+                            </a>
+
+                        </td>
                     </tr>
                     
                     
@@ -103,7 +106,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Create</button>
+                <button type="submit" class="btn btn-primary">Create</button>
             </div>
         </form>
 
@@ -118,6 +121,7 @@
     export default {
         data() { 
             return {
+                users: {},
                 form: new Form({
                     name: '',
                     email: '',
@@ -129,13 +133,16 @@
             }
         },
         methods: {
+            loadUsers(){
+                axios.get("api/user").then(({ data }) => (this.users = data.data));
+            },
             createUser(){
                 this.form.post('api/user');
             }
 
         },
-        mounted() {
-            console.log('Component mounted.')
+        created() {
+            this.loadUsers();
         }
     }
 </script>
