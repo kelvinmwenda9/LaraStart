c2d37786158fb004b47cb2ec5be5fa058ba4c9b4
kelvinmwenda9 <kelvinmwenda9@gmail.com>
2020-05-06 09:13:20 +0300

    Form Validation

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -50,7 +50,6 @@
             <!-- /.card -->
           </div>
         </div>
-        <form action="">
         <!-- Modal -->
         <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -61,6 +60,8 @@
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
+
+        <form @submit="createUser">
             <div class="modal-body">
 
                  <div class="form-group">
@@ -99,17 +100,13 @@
                         class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
                         <has-error :form="form" field="password"></has-error>
                     </div>
-
-
-
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary">Create</button>
             </div>
-
-            </form>
-            
+        </form>
+        
             </div>
         </div>
         </div>
