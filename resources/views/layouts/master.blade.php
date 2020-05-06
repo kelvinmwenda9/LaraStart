054e19f50930cab89aeb6194aa2eb51e6b05b88a
kelvinmwenda9 <kelvinmwenda9@gmail.com>
2020-05-06 08:46:18 +0300

    Table, Users Form, Buttons, Dashboard, Management

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -73,7 +73,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
             <li class="nav-item">
                 <router-link to="/dashboard" class="nav-link">
-                  <i class="nav-icon fas fa-tachometer-alt"></i>
+                  <i class="nav-icon fas fa-tachometer-alt blue"></i>
                     <p>
                         Dashboard
                     </p>
@@ -82,7 +82,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link active">
-              <i class="nav-icon fa fa-cog"></i>
+              <i class="nav-icon fa fa-cog green"></i>
               <p>
                 Management
                 <i class="right fas fa-angle-left"></i>
@@ -90,10 +90,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
+                <router-link to="/users" class="nav-link active">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Users</p>
+                </router-link>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
@@ -106,7 +106,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
           <li class="nav-item">
                 <router-link to="/profile" class="nav-link">
-                  <i class="nav-icon fas fa-user"></i>
+                  <i class="nav-icon fas fa-user yellow"></i>
                     <p>
                         Profile
                     </p>
@@ -114,12 +114,20 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </li>
 
             <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="nav-icon fa fa-power-off"></i>
+
+                <a class="nav-link" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();">
+                    <i class="nav-icon fa fa-power-off red"></i>
                     <p>
-                        Logout
+                    {{ __('Logout') }}
                     </p>
+
                 </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
             </li>
          
         </ul>
